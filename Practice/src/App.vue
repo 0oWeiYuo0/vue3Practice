0579dfcaa4f2c40358db4dbe6d9ffdd62a732fe4
0oWeiYuo0 <s43210712@gmail.com>
2023-11-12 15:40:48 +0800

    複習Vue

---
+++ Practice/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import { ref } from "vue";
+
+const count = ref(0);
+
+const add = () => {
+  count.value++;
+}
+
+const increment = () => {
+  count.value = count.value + 2;
+}
+
+
+</script>
+
+<template>
+  <div>
+    <h1>Vue: {{ count }}</h1>
+    <button @click="add">加一</button>
+    <button @click="increment">加二</button>
+  </div>
+</template>
+
+<style></style>
